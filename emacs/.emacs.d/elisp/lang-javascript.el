d857340d488c4fe8fd61ba72d1b16cfae0e7efd5
Martin Øinæs Myrseth <myrseth@gmail.com>
2017-01-07 13:02:54 +0100

    emacs: Update JavaScript settings

--- emacs/.emacs.d/elisp/lang-javascript.el
+++ emacs/.emacs.d/elisp/lang-javascript.el
@@ -26,6 +26,7 @@
   ;; tern :- IDE like features for javascript and completion
   ;; http://ternjs.net/doc/manual.html#emacs
   (use-package tern
+    :ensure t
     :config
     (defun my-js-mode-hook ()
       "Hook for `js-mode'."
@@ -38,21 +39,34 @@
 
   ;; company backend for tern
   ;; http://ternjs.net/doc/manual.html#emacs
-  (use-package company-tern)
+  (use-package company-tern :ensure t)
 
   ;; Run a JavaScript interpreter in an inferior process window
   ;; https://github.com/redguardtoo/js-comint
   (use-package js-comint
+    :ensure t
     :config
     (setq inferior-js-program-command "node"))
 
   ;; js2-refactor :- refactoring options for emacs
   ;; https://github.com/magnars/js2-refactor.el
   (use-package js2-refactor
+    :ensure t
     :defer t
     :diminish js2-refactor-mode
     :config
     (js2r-add-keybindings-with-prefix "C-c j r"))
   (add-hook 'js2-mode-hook 'js2-refactor-mode))
+
+  (defun my/use-eslint-from-node-modules ()
+    (let ((root (locate-dominating-file
+                 (or (buffer-file-name) default-directory)
+                 (lambda (dir)
+                   (let ((eslint (expand-file-name "node_modules/.bin/eslint" dir)))
+                     (and eslint (file-executable-p eslint)))))))
+      (when root
+        (let ((eslint (expand-file-name "node_modules/.bin/eslint" root)))
+          (setq-local flycheck-javascript-eslint-executable eslint)))))
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 (provide 'lang-javascript)
