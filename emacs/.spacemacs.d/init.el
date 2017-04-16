d08fc9ad3e88b0427e00c88a71b1cbf4173f11a1
Martin Øinæs Myrseth <myrseth@gmail.com>
2017-04-16 13:15:54 +0200

    emacs: Add "insert date" keybinding + function

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -329,6 +329,13 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; Date
+  (defun my/date ()
+    (interactive)
+    (insert (format-time-string "%Y-%m-%d")))
+
+  (spacemacs/set-leader-keys "id" #'my/date)
+
   ;; Evil escape
   (setq-default evil-escape-key-sequence "qq")
 
