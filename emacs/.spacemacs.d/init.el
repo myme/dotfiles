aa28ae6c9a8d15272c1a916ebbfeeecd4082f709
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2017-05-08 11:42:32 +0200

    emacs: Add rename-buffer binding

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -329,6 +329,9 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; Buffers
+  (spacemacs/set-leader-keys "b," #'rename-buffer)
+
   ;; Date
   (defun my/date ()
     (interactive)
