83466a6bc8bc6ac9d57fe6096818747130c51ac5
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2017-09-26 09:11:21 +0200

    emacs: Remove custom-set-faces

--- emacs/.spacemacs.d/custom.el
+++ emacs/.spacemacs.d/custom.el
@@ -102,4 +102,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:foreground "#ffffff" :background "#263238")))))
+ )
