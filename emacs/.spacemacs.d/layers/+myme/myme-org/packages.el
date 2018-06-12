e3eafdf10f49b92a6487a5d759a02d3876a912c1
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-06-12 21:36:03 +0200

    emacs: Remove spacemacs

--- emacs/.spacemacs.d/layers/+myme/myme-org/packages.el
+++
@@ -1,9 +0,0 @@
-(defconst myme-org-packages nil)
-
-(spacemacs|use-package-add-hook org
-  :post-config
-  (progn
-    (add-to-list 'org-modules 'org-habit)
-    (setq
-     org-directory "~/Dropbox/org"
-     org-default-notes-file (expand-file-name "notes.org" "~/Dropbox/org/notes.org"))))
