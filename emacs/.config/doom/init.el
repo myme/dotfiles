e2421917bb74b358ea3871249c4500081e974ca8
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-06-13 10:39:20 +0200

    emacs: Disable splash buffer for now

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -27,7 +27,7 @@
 
        :ui
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+      ;doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
