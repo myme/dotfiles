bb9a6e7bc99003294d1dba9b37855c722f442e87
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-06-13 14:17:47 +0200

    emacs: Add myme-dashboard (fork of doom-dashboard)

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -142,7 +142,10 @@
        ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
        ;; and additional ex commands for evil-mode. Use it as a reference for
        ;; your own modules.
-       (default +bindings +snippets +evil-commands))
+       (default +bindings +snippets +evil-commands)
+       
+       :private
+       myme-dashboard)
 
 (setq doom-font (font-spec :family "DejaVu Sans Mono" :size 12))
 
