2d03d4af6d4f00cef440154fcfe3924d74823ca7
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-06-14 20:23:43 +0200

    emacs: Add async projectile command binding

--- emacs/.config/doom/modules/private/myme-config/config.el
+++ emacs/.config/doom/modules/private/myme-config/config.el
@@ -13,6 +13,8 @@
         "C-/" #'counsel-company))
 
 (map! :leader
+      (:prefix "p"
+        :desc "Run async cmd in project" :nv "&" #'projectile-run-async-shell-command-in-root)
       (:desc "theme" :prefix "T"
         :desc "Switch theme" :nv "T" #'counsel-load-theme))
 
