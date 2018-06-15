2c71067370f97d90189b351f64b575ea37943648
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-06-15 09:19:27 +0200

    emacs: Add Org Calendar navigation bindings

--- emacs/.config/doom/modules/private/myme-org/+keybindings.el
+++ emacs/.config/doom/modules/private/myme-org/+keybindings.el
@@ -1,9 +1,22 @@
 ;;; private/myme-org/+keybindings.el -*- lexical-binding: t; -*-
 
-(map! :leader
-      (:prefix "o"
-        (:desc "Org Agenda" :prefix "a"
-            :desc "Org Agenda" :nv "a" #'org-agenda
-            :desc "Org Default Agenda" :nv "d" #'myme/org-agenda-default-list
-          )))
+(map!
+ (:leader
+  (:prefix "o"
+    (:desc "Org Agenda" :prefix "a"
+      :desc "Org Agenda" :nv "a" #'org-agenda
+      :desc "Org Default Agenda" :nv "d" #'myme/org-agenda-default-list
+      )))
+ (:after org
+   (:map org-read-date-minibuffer-local-map
+     ;; Calendar navigation
+     "M-h" (lambda! (org-eval-in-calendar '(calendar-backward-day 1)))
+     "M-l" (lambda! (org-eval-in-calendar '(calendar-forward-day 1)))
+     "M-k" (lambda! (org-eval-in-calendar '(calendar-backward-week 1)))
+     "M-j" (lambda! (org-eval-in-calendar '(calendar-forward-week 1)))
+     "M-H" (lambda! (org-eval-in-calendar '(calendar-backward-month 1)))
+     "M-L" (lambda! (org-eval-in-calendar '(calendar-forward-month 1)))
+     "M-K" (lambda! (org-eval-in-calendar '(calendar-backward-year 1)))
+     "M-J" (lambda! (org-eval-in-calendar '(calendar-forward-year 1)))
+     )))
 
