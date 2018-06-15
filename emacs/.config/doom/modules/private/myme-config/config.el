4637acf2f0ab91c0adec59450db147108b164f22
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-06-16 00:10:09 +0200

    emacs: Remove tab hack

--- emacs/.config/doom/modules/private/myme-config/config.el
+++ emacs/.config/doom/modules/private/myme-config/config.el
@@ -45,7 +45,3 @@
    'counsel-projectile-switch-project
    '(("v" magit-status "version control"))))
 
-;; Fix TAB issues made with :config default/+bindings.el
-(if (not window-system)
-    (define-key input-decode-map (kbd "TAB") nil))
-
