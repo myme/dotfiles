211065973cf1414ff673ec08822fe316c00b3a23
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-06-16 22:46:47 +0200

    emacs: Add some comments

--- emacs/.config/doom/modules/private/myme-config/config.el
+++ emacs/.config/doom/modules/private/myme-config/config.el
@@ -15,14 +15,16 @@
  ;; Dired
  dired-dwim-target t)
 
-;; Bindings
+;; Bindings - TODO this doesn't work optimally as selecting a candidate often breaks the existing text
 (map! (:after company
         :map company-mode-map
         "C-/" #'counsel-company))
 
+;; Ace window
 (after! ace-window
   (setq aw-scope 'global))
 
+;; Jump bindings
 (after! evil
   (require 'evil-collection-avy)
   (map! :leader
@@ -30,6 +32,7 @@
           :desc "Jump to character" :nv "c" #'evil-avy-goto-char
           :desc "Jump to line" :nv "l" #'evil-avy-goto-line)))
 
+;; Custom leader bindings
 (map! :leader
       (:prefix "b"
         :desc "Rename buffer" :nv "r" #'rename-buffer)
