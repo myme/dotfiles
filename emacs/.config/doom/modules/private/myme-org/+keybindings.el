75a91637a155c794db263a55b066918da45632a0
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-07-05 15:23:00 +0200

    emacs: Latest doom develop + org keybindings

--- emacs/.config/doom/modules/private/myme-org/+keybindings.el
+++ emacs/.config/doom/modules/private/myme-org/+keybindings.el
@@ -3,7 +3,7 @@
 (map!
  (:leader
   (:prefix "o"
-    (:desc "Org Agenda" :prefix "a"
+    (:desc "Org Agenda" :prefix "o"
       :desc "Org Agenda" :nv "a" #'org-agenda
       :desc "Org Default Agenda" :nv "d" #'myme/org-agenda-default-list
       )))
