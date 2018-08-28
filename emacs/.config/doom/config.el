1267ab4357df8b65335d34f49f22ad7fdd374475
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-08-28 13:07:57 +0200

    emacs: Transparency

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -5,6 +5,9 @@
 (set-popup-rules!
   '(("^\\*Async"  :slot 1 :vslot -1 :ttl 0)))
 
+(set-frame-parameter (selected-frame) 'alpha 95)
+(add-to-list 'default-frame-alist '(alpha  95))
+
 (setq
  ;; Look-and-feel
  doom-font (font-spec :family "DejaVu Sans Mono" :size 12)
