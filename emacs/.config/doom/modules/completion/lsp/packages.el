d5b86e1cd932f63d29e7a941804de5240430bee5
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2018-12-04 09:18:32 +0100

    emacs: Simplify lsp module

--- emacs/.config/doom/modules/completion/lsp/packages.el
+++ emacs/.config/doom/modules/completion/lsp/packages.el
@@ -5,39 +5,6 @@
   (package! lsp-ui)
   (package! company-lsp)
 
-  (when (featurep! +javascript)
-    (package! lsp-typescript
-      :recipe (:fetcher
-               github
-               :repo "emacs-lsp/lsp-javascript"
-               :files ("lsp-typescript.el")))
-    (package! tide :disable t))
-
-  (when (featurep! +go)
-    (package! lsp-go))
-
-  (when (featurep! +python)
-    (package! anaconda-mode :disable t))
-
-  (when (featurep! +rust)
-    (package! lsp-rust)
-    (package! racer :disable t))
-
-  (when (featurep! +css)
-    (package! lsp-css
-      :recipe (:fetcher
-               github
-               :repo "emacs-lsp/lsp-css")))
-
   (when (featurep! +cpp)
     (package! cquery)
-    (package! rtags :disable t))
-
-  (when (featurep! +ocaml)
-    (package! lsp-ocaml))
-
-  (when (featurep! +java)
-    (package! lsp-intellij))
-
-  (when (featurep! +sh)
-    (package! company-shell :disable t)))
+    (package! rtags :disable t)))
