22bba9ed8b4852fbadcf89c783bea2f95c81c2e4
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-08-19 11:34:54 +0200

    emacs: Add lsp to cc mode

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -97,7 +97,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;;cc                ; C/C++/Obj-C madness
+       (cc +lsp)           ; C/C++/Obj-C madness
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
