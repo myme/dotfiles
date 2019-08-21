b058e43c39e2cbf0e5223c91c91ce8fc1d8a4850
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-08-21 09:09:34 +0200

    zsh: Remove prezto (add pure prompt)
    See how far we'll get with plain zsh

--- nix/.config/nixpkgs/overlays.nix
+++ nix/.config/nixpkgs/overlays.nix
@@ -3,7 +3,27 @@ let
     baseEnv = self.buildEnv {
       name = "env-base";
       paths = with self; [
+        pureZshPrompt
       ];
+    };
+
+    pureZshPrompt = with self; stdenv.mkDerivation rec {
+      name = "zsh-pure-prompt-${version}";
+      version = "v1.10.3";
+      rev = "${version}";
+
+      src = fetchFromGitHub {
+        inherit rev;
+        owner = "sindresorhus";
+        repo = "pure";
+        sha256 = "0zjgnlw01ri0brx108n6miw4y0cxd6al1bh28m8v8ygshm94p1zx";
+      };
+
+      installPhase = ''
+        mkdir -p $out/share/zsh/site-functions
+        install $src/pure.zsh $out/share/zsh/site-functions/prompt_pure_setup
+        install $src/async.zsh $out/share/zsh/site-functions/async
+      '';
     };
   };
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,10 @@
-.zprezto/runcoms/zshrc
+# Pure prompt
+fpath=("$HOME/.nix-profile/share/zsh/site-functions" $fpath)
+export PURE_GIT_PULL=0
+export RPS1=""
+autoload -U promptinit; promptinit
+prompt pure
+
+# Aliases
+alias ta="tmux attach -t"
+alias tl="tmux list-sessions"
