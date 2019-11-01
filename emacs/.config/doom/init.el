a883beb2122d5548e13cba09a07b388f5947ee65
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-11-01 12:57:19 +0100

    emacs: Add flyspell

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -77,7 +77,7 @@
        ;;ein             ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
        flycheck          ; tasing you for every semicolon you forget
-       ;;flyspell          ; tasing you for misspelling mispelling
+       flyspell          ; tasing you for misspelling mispelling
        ;;gist            ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
