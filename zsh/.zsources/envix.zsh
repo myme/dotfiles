574d4715cabd3fdf0fbbafc505ded4783618ce08
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2019-11-18 10:40:33 +0100

    zsh: envix: Update envix functions

--- zsh/.zsources/envix.zsh
+++ zsh/.zsources/envix.zsh
@@ -1,9 +1,14 @@
-# Envix (pd: project cd)
-pd () {
+# Envix (p: project cd)
+p () {
     local project;
     project=$(envix -s "$@" | tail -1)
     if [ -z "$project" ]; then
         return
     fi
     cd "$project"
+}
+
+# Envix (x: project exec)
+x () {
+    envix . "$@"
 }
