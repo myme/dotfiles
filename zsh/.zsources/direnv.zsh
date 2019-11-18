5cf7808e1fd11d99dd2b1feb22201c852e4147db
Martin Øinæs Myrseth <mmyrseth@cisco.com>
2019-11-18 10:39:23 +0100

    zsh: Add direnv-start/-stop

--- zsh/.zsources/direnv.zsh
+++ zsh/.zsources/direnv.zsh
@@ -1,2 +1,27 @@
 # Direnv
-eval "$(direnv hook zsh)"
+
+# Output from: direnv hook zsh
+# E.g. eval "$(direnv hook zsh)"
+_direnv_hook_enabled=1
+_direnv_hook() {
+    if [ $_direnv_hook_enabled -eq 1 ]; then
+        eval "$(direnv export zsh)";
+    fi
+}
+
+direnv-stop() {
+    pushd > /dev/null
+    _direnv_hook
+    _direnv_hook_enabled=0
+    popd > /dev/null
+}
+
+direnv-start() {
+    _direnv_hook_enabled=1
+}
+
+typeset -ag precmd_functions;
+
+if [[ -z ${precmd_functions[(r)_direnv_hook]} ]]; then
+  precmd_functions+=_direnv_hook;
+fi
