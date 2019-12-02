e7766ec514164fd6ae4691ad63e3ea48d91e86b7
Martin Øinæs Myrseth <myrseth@gmail.com>
2019-12-02 08:18:24 +0100

    emacs: Add rust module

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -142,7 +142,7 @@
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
