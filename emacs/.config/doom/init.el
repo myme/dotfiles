f46c527953282ea2a0a84f413419b466c467a229
Martin Øinæs Myrseth <myrseth@gmail.com>
2020-09-10 08:59:10 +0200

    emacs: Enable REST client

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -155,7 +155,7 @@
        (python +lsp)       ; beautiful is better than ugly
        qt                  ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
