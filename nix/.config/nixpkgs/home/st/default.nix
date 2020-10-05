a8ad3d260d6509f686decb46efe005cc7f0b8c65
Martin Øinæs Myrseth <myrseth@gmail.com>
2020-10-05 21:44:31 +0200

    st: Saving WIP with patches

--- nix/.config/nixpkgs/home/st/default.nix
+++ nix/.config/nixpkgs/home/st/default.nix
@@ -5,6 +5,13 @@
 
 pkgs.st.override {
   patches = [
+    # ./st-alpha-0.8.2.diff
+    # ./st-desktopentry-0.8.2.diff
+    # ./st-selectioncolors-0.8.2.diff
+    # (builtins.fetchurl {
+    #   url = "https://st.suckless.org/patches/selectioncolors/st-selectioncolors-0.8.2.diff";
+    #   sha256 = "195aicbwwl3iz71mi6gcknbgsymvd7vaz2fkia0bjw8a1bmwn7hn";
+    # })
     (builtins.fetchurl {
       url = "https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff";
       sha256 = "11dj1z4llqbbki5cz1k1crr7ypnfqsfp7hsyr9wdx06y4d7lnnww";
