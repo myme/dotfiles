bb0d9657d20373b3322aabc8699a521282c65c90
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-18 20:24:00 +0100

    nixos: SSH with terminal allocation

--- bootstrap/ssh.sh
+++ bootstrap/ssh.sh
@@ -5,4 +5,4 @@ NIX_PORT=${NIX_PORT:-2222}
 NIX_USER=${NIX_USER:-root}
 SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
-ssh $NIX_HOST -p$NIX_PORT -l$NIX_USER $SSH_OPTS "$@"
+ssh -tt $NIX_HOST -p$NIX_PORT -l$NIX_USER $SSH_OPTS "$@"
