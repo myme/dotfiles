428c6dc5c553b2b55679732a4c5585c9186b7857
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-18 20:33:06 +0100

    nixos: Format + add empty devShell

--- flake.nix
+++ flake.nix
@@ -4,26 +4,33 @@
     home-manager.url = "github:nix-community/home-manager/release-21.11";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager }:
+    let
       system = "x86_64-linux";
-      modules = [
-        ./machine
-        ./users
-        home-manager.nixosModules.home-manager
-        {
-          # Home manager
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./machine
+          ./users
+          home-manager.nixosModules.home-manager
+          {
+            # Home manager
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
 
-          # Let 'nixos-version --json' know about the Git revision
-          # of this flake.
-          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision =
+              nixpkgs.lib.mkIf (self ? rev) self.rev;
 
-          # Pin flake nixpkgs
-          nix.registry.nixpkgs.flake = nixpkgs;
-        }
-      ];
+            # Pin flake nixpkgs
+            nix.registry.nixpkgs.flake = nixpkgs;
+          }
+        ];
+      };
+
+      devShell.${system} = pkgs.mkShell { };
     };
-  };
 }
