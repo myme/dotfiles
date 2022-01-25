99202f696b89684edadaac708a207a43d66af045
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-25 21:21:45 +0100

    flake: Ignore machines/default.nix
    It is not meant to be built.

--- lib/allMachines.nix
+++ lib/allMachines.nix
@@ -4,7 +4,10 @@
 
 f:
 
-builtins.listToAttrs (builtins.map (fname: rec {
+let
+  allMachines = builtins.filter (m: m != "default.nix") (pkgs.myme.lib.allNixFiles ../machines);
+
+in builtins.listToAttrs (builtins.map (fname: rec {
   name = lib.strings.removeSuffix ".nix" fname;
   value = f name ../machines/${fname};
-}) (pkgs.myme.lib.allNixFiles ../machines))
+}) allMachines)
