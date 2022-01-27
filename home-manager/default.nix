c5e4be02807e4cfb33b987fe086cede1ddf85197
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-27 12:36:14 +0100

    mime: Firefox as default browser

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -74,5 +74,18 @@
         client.enable = true;
       };
     };
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["firefox.desktop"];
+        "text/xml" = ["firefox.desktop"];
+        "application/xhtml+xml" = ["firefox.desktop"];
+        "application/vnd.mozilla.xul+xml" = ["firefox.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/ftp" = ["firefox.desktop"];
+      };
+    };
   };
 })
