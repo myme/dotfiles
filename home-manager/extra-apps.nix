72732d8ca07b4013d8170cac8ebeee4a592a349d
Martin Øinæs Myrseth <myme@Tuple.localdomain>
2022-03-06 22:13:24 +0100

    mime: Move to extra-apps
    Only relevant when installing Firefox.

--- home-manager/extra-apps.nix
+++ home-manager/extra-apps.nix
@@ -17,4 +17,17 @@
     gnome3.evince
     gnome3.seahorse
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = ["firefox.desktop"];
+      "text/xml" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
+      "application/vnd.mozilla.xul+xml" = ["firefox.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "x-scheme-handler/ftp" = ["firefox.desktop"];
+    };
+  };
 }
