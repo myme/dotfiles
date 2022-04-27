415e27786047a054ea653050a59fb21cbf061adb
Martin Myrseth <mm@myme.no>
2022-04-27 12:17:26 +0200

    emacs: Add elm mode

--- home-manager/emacs/doom/init.el
+++ home-manager/emacs/doom/init.el
@@ -124,7 +124,7 @@
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
-       ;;elm               ; care for a cup of TEA?
+       (elm +lsp)        ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
