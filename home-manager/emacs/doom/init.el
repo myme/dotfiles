da0722eeb25f83fd284b3e88fbb7a85a80945472
Martin Myrseth <mm@myme.no>
2022-04-27 12:17:13 +0200

    emacs: Add JSON mode

--- home-manager/emacs/doom/init.el
+++ home-manager/emacs/doom/init.el
@@ -138,7 +138,7 @@
        (haskell +lsp)      ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp)   ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
