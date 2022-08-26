ccf97aab26b9d067fe3d559f48032fae72dfbdcd
Martin Myrseth <mm@myme.no>
2022-08-26 19:51:47 +0200

    emacs: Move bindings into private org module

--- home-manager/emacs/doom/modules/private/myme-org/+keybindings.el
+++ home-manager/emacs/doom/modules/private/myme-org/+keybindings.el
@@ -8,7 +8,10 @@
    (:prefix "o" :desc "Org Agenda"
     :desc "Org Agenda" :nv "a" #'org-agenda
     :desc "Org Clock In" :nv "i" #'myme/org-clock-in-recent
-    :desc "Org Clock Out" :nv "o" #'org-clock-out)
+    :desc "Org Clock Out" :nv "o" #'org-clock-out
+    :desc "Set org-mode variables to project" :nv "p" #'myme/org-select-project
+    :desc "Visit all org-mode files" :nv "r" #'myme/org-element-cache-reset-all
+    :desc "Visit all org-mode files" :nv "v" #'myme/org-visit-all)
    (:prefix "s" :desc "Org Sync"
     (:desc "Org sync all" :nv "s" #'myme/org-auto-sync))
    )))
