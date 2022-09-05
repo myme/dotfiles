e6e3d8fda833ccd525a07df572c81c08c21b5d40
Martin Myrseth <mm@myme.no>
2022-09-05 13:49:25 +0200

    wm: Create my own i3lock-fancy
    Seems like dunst notifications appear over the i3lock-fancy lockscreen.
    This changes just does the same manual steps of grabbing the screen,
    pixelating and locking with an image.
    
    See: eb05f47ecf04feac3b01b9366b3dfa7c82e7c1f7

--- apps/lockscreen/default.nix
+++ apps/lockscreen/default.nix
@@ -1,12 +1,24 @@
-{ i3lock-fancy, writeShellScriptBin }:
+{ i3lock, imagemagick, scrot, writeShellScriptBin }:
 
 writeShellScriptBin "lockscreen" ''
-  ${i3lock-fancy}/bin/i3lock-fancy --pixelate
-  PID=$!
+  bg_image="''${XDG_RUNTIME_DIR:-/tmp}/lock-image.png"
 
-  while kill ''${PID} -0 ; do
-    if timeout 5 fprintd-verify; then
-      kill ''${PID}
-    fi
-  done
+  # Capture screen
+  ${scrot}/bin/scrot -o $bg_image
+
+  # Pixelate
+  ${imagemagick}/bin/convert -scale 10% -scale 1000% $bg_image $bg_image
+
+  # Lock
+  ${i3lock}/bin/i3lock -ni $bg_image
+
+  # PID=$!
+  # while kill ''${PID} -0 ; do
+  #   if timeout 5 fprintd-verify; then
+  #     kill ''${PID}
+  #   fi
+  # done
+
+  # Remove image on exit
+  rm -f $bg_image
 ''
