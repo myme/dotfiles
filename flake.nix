28227fff2fe01d4fe786b71f4472ec87486d2242
Martin Myrseth <mm@myme.no>
2022-11-08 20:56:25 +0100

    nixos-wsl: Switch to fork with login shell support
    This is a temporary switch to my NixOS-WSL fork which adds rudimentary
    login shell behavior support to WSL.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    # nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.url = "github:myme/NixOS-WSL/login-shell";
     flake-utils.url = "github:numtide/flake-utils";
     agenix = {
       url = "github:ryantm/agenix";
--- system/default.nix
+++ system/default.nix
@@ -75,12 +75,20 @@
     })
     # Enable NixOS-WSL module
     (lib.mkIf (config.myme.machine.flavor == "wsl") {
-      wsl = {
-        enable = true;
-        automountPath = "/mnt";
-        defaultUser = config.myme.machine.user.name;
-        startMenuLaunchers = false; # Done below to include Home Manager apps
-      };
+      wsl =
+        let
+          username = config.myme.machine.user.name;
+
+        in {
+          enable = true;
+          defaultUser = username;
+          loginShell =
+            let shell = config.users.users.${username}.shell;
+                shellPath = "${shell}${shell.shellPath}";
+            in "${shellPath} --login -c";
+          startMenuLaunchers = false; # Done below to include Home Manager apps
+          wslConf.automount.root = "/mnt";
+        };
 
       # Copied from https://github.com/nix-community/NixOS-WSL/blob/69783cf56b2ada7e0e8cc8d17907a346e8bd97b7/modules/wsl-distro.nix#L111
       system.activationScripts.copy-home-launchers = lib.stringAfter [ ] ''
