5ab262729d805c6da97397c6fa80b90ff7c2e0ab
Martin Myrseth <mm@myme.no>
2022-11-13 12:32:19 +0100

    flake: Refactor out hardcoded system
    This commit is a bunch of changes to let each machine configuration
    state it's system architecture to accommodate for non-x86_64 machines.
    This involves wrapping all machine configurations in an addiditional
    attribute set of the form `{ system, config }` where the old config has
    been moved under the `config` key.

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,9 @@
-{ lib, home-manager }:
+{ lib, inputs, overlays }:
 {
   myme = {
     allNixFiles = import ./allNixFiles.nix { inherit lib; };
     allProfiles = import ./allProfiles.nix { inherit lib; };
-    nixos2hm = import ./nixos2hm.nix { inherit lib home-manager; };
-    makeNixOS = import ./makeNixOS.nix;
+    nixos2hm = import ./nixos2hm.nix { inherit lib; };
+    makeNixOS = import ./makeNixOS.nix { inherit inputs overlays; };
   };
 }
