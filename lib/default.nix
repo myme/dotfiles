23e545e3873f7a8f117a843bda2946060421b9e4
Martin Myrseth <mm@myme.no>
2022-11-20 16:19:52 +0100

    lib: Add allProfilesIf
    Allow mapping over all machine configs with a predicate function.

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,12 @@
 { lib, inputs, overlays }:
-{
+let
+  allProfilesIf = import ./allProfilesIf.nix { inherit lib; };
+
+in {
   myme = {
     allNixFiles = import ./allNixFiles.nix { inherit lib; };
-    allProfiles = import ./allProfiles.nix { inherit lib; };
+    allProfiles = allProfilesIf (_: _: true);
+    inherit allProfilesIf;
     nixos2hm = import ./nixos2hm.nix { inherit lib; };
     makeNixOS = import ./makeNixOS.nix { inherit inputs overlays; };
   };
