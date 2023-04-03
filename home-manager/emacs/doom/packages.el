7edac3c808a4995ec60ca6f51ec988230b42fd28
Martin Myrseth <mm@myme.no>
2023-04-03 13:43:25 +0200

    emacs: The age of A.I. (add "ChatGPT")

--- home-manager/emacs/doom/packages.el
+++ home-manager/emacs/doom/packages.el
@@ -2,6 +2,8 @@
 ;;; ~/dotfiles/emacs/.config/doom/packages.el
 
 ;; Misc
+(package! chatgpt-shell
+  :recipe (:host github :repo "xenodium/chatgpt-shell"))
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
 (package! keychain-environment)
