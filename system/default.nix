3b8fbbc954685e0eec6e94edd48dbd1c9c9bcb01
Martin Myrseth <mm@myme.no>
2023-04-19 21:57:04 +0200

    nixfmt

--- system/default.nix
+++ system/default.nix
@@ -1,10 +1,6 @@
 # Global system configuration
 { config, lib, pkgs, system, ... }: {
-  imports = [
-    ./sleep.nix
-    ./xserver.nix
-    ./users.nix
-  ];
+  imports = [ ./sleep.nix ./xserver.nix ./users.nix ];
 
   options.myme.machine = {
     name = lib.mkOption {
@@ -54,8 +50,8 @@
       # GnuPG - enable if enabled for any user
       programs.gnupg.agent = {
         enable = with builtins;
-          any (x: x)
-            (map (x: x.services.gpg-agent.enable) (attrValues config.home-manager.users));
+          any (x: x) (map (x: x.services.gpg-agent.enable)
+            (attrValues config.home-manager.users));
         enableSSHSupport = true;
       };
 
@@ -65,9 +61,8 @@
         extraOptions = "experimental-features = nix-command flakes";
         settings = {
           trusted-users = [ "root" config.myme.machine.user.name ];
-          trusted-public-keys = [
-            "Tuple:FRwemNd0zmxD24+XgQRibsfNH5Vl32rOdc0NWvtJLYE="
-          ];
+          trusted-public-keys =
+            [ "Tuple:FRwemNd0zmxD24+XgQRibsfNH5Vl32rOdc0NWvtJLYE=" ];
         };
       };
 
@@ -89,18 +84,16 @@
     })
     # Enable NixOS-WSL module
     (lib.mkIf (config.myme.machine.flavor == "wsl") {
-      wsl =
-        let
-          username = config.myme.machine.user.name;
+      wsl = let username = config.myme.machine.user.name;
 
-        in {
-          enable = true;
-          defaultUser = username;
-          interop.register = true;
-          # nativeSystemd = true;
-          startMenuLaunchers = false; # Done below to include Home Manager apps
-          wslConf.automount.root = "/mnt";
-        };
+      in {
+        enable = true;
+        defaultUser = username;
+        interop.register = true;
+        # nativeSystemd = true;
+        startMenuLaunchers = false; # Done below to include Home Manager apps
+        wslConf.automount.root = "/mnt";
+      };
 
       # Copied from https://github.com/nix-community/NixOS-WSL/blob/69783cf56b2ada7e0e8cc8d17907a346e8bd97b7/modules/wsl-distro.nix#L111
       system.activationScripts.copy-home-launchers = lib.stringAfter [ ] ''
