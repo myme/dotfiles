7bfe6c72cbd3043cffbb1c87463491c2ba9b4b67
Martin Myrseth <mm@myme.no>
2023-07-06 10:34:14 +0200

    wsl: Switch to nativeSystemd
    There is one strange issue occurring on my tablet which doesn't happen
    on my desktop: https://gist.github.com/myme/99f009454001e2ec1d1d8de193f34c25

--- system/default.nix
+++ system/default.nix
@@ -90,22 +90,8 @@
         enable = true;
         defaultUser = username;
         interop.register = true;
-        # nativeSystemd = true;
-        startMenuLaunchers = false; # Done below to include Home Manager apps
-        wslConf.automount.root = "/mnt";
+        nativeSystemd = true;
       };
-
-      # Copied from https://github.com/nix-community/NixOS-WSL/blob/69783cf56b2ada7e0e8cc8d17907a346e8bd97b7/modules/wsl-distro.nix#L111
-      system.activationScripts.copy-home-launchers = lib.stringAfter [ ] ''
-        cd "$(mktemp -d)"
-        for x in applications icons; do
-          echo "Copying /usr/share/$x"
-          ${pkgs.rsync}/bin/rsync -ar $systemConfig/sw/share/$x/. ./$x
-          ${pkgs.rsync}/bin/rsync -ar /etc/profiles/per-user/${config.myme.machine.user.name}/share/$x/. ./$x
-          mkdir -p /usr/share/$x
-          ${pkgs.rsync}/bin/rsync -ar --delete ./$x/. /usr/share/$x
-        done
-      '';
     })
     (lib.mkIf (config.myme.machine.role != "server") {
       # For GTK stuff
