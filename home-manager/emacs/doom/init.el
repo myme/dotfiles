5c026926759ba33b247e7da1d8b98ccb84814899
Martin Myrseth <mm@myme.no>
2023-08-01 12:22:34 +0200

    emacs: Sync init.el template with doomemacs

--- home-manager/emacs/doom/init.el
+++ home-manager/emacs/doom/init.el
@@ -88,6 +88,7 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        direnv
        docker
