eabdfb0e71af7cefd03faade8eed9d0773effbdb
Martin Myrseth <myrseth@gmail.com>
2023-10-21 23:32:51 +0200

    irc: Patch weechat-matrix
    The latest release and what's in `nixpkgs` is `v0.3.0`. However, with
    the update of `matrix-nio` in `pythonPackages` there are
    incompatibilities that were resolved with:
    
    https://github.com/poljar/weechat-matrix/commit/feae9fda26ea9de98da9cd6733980a203115537e
    
    This change just patches up `weechat-matrix` by pulling in the latest.

--- home-manager/irc.nix
+++ home-manager/irc.nix
@@ -4,10 +4,21 @@ let
   cfg = config.myme.irc;
   weechat = pkgs.weechat.override {
     configure = { availablePlugins, ... }: {
-      scripts = with pkgs.weechatScripts; [
-        wee-slack
-        weechat-matrix
-      ];
+      scripts =
+        let ps = pkgs.weechatScripts;
+        in [
+          ps.wee-slack
+          (ps.weechat-matrix.overrideAttrs (attrs: attrs // {
+            version = "0.3.0-patched";
+            src = pkgs.fetchFromGitHub {
+              owner = "poljar";
+              repo = "weechat-matrix";
+              rev = "feae9fda26ea9de98da9cd6733980a203115537e";
+              hash = "sha256-flv1XF0tZgu3qoMFfJZ2MzeHYI++t12nkq3jJkRiCQ0=";
+            };
+            patches = [];
+          }))
+        ];
     };
   };
   tmuxCmd = "${pkgs.tmux}/bin/tmux -L weechat";
