9068a992dd18be29287033b5bb52c9471696f7f3
Martin Myrseth <mm@myme.no>
2023-10-27 08:40:26 +0200

    xorg: Stop/start `dunst` on screen lock

--- home-manager/wm/lock-wrapper.sh
+++ home-manager/wm/lock-wrapper.sh
@@ -5,7 +5,7 @@ set -eu
 # Stop compositor during lock due to issues with application content being
 # painted over the lock screen: https://github.com/i3/i3lock/issues/204
 
-systemctl --user stop picom.service
-trap "systemctl --user start picom.service" EXIT
+systemctl --user stop dunst.service picom.service
+trap "systemctl --user start dunst.service picom.service" EXIT
 
 "$@"
