85343e28abcec9c150a638fe7e9c255317c4a314
Martin Myrseth <mm@myme.no>
2023-12-03 22:06:35 +0100

    vscode: Actually *use* enable config

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,13 +1,16 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  cfg = config.myme.dev.vscode;
+
+in {
   options.myme.dev.vscode = {
     enable = lib.mkEnableOption "Enable VS Code";
   };
 
   config = {
     programs.vscode = {
-      enable = true;
+      enable = cfg.enable;
       extensions = [
         pkgs.vscode-extensions.mkhl.direnv
         pkgs.vscode-extensions.rust-lang.rust-analyzer
