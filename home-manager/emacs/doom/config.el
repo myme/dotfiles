52501104b4de2c235d291cbd04e0d05cc5fa727a
Martin Myrseth <mm@myme.no>
2025-06-09 13:52:18 +0200

    emacs: Default to fresher Claude for gptel

--- home-manager/emacs/doom/config.el
+++ home-manager/emacs/doom/config.el
@@ -296,7 +296,7 @@
 (use-package! gptel
   :config
   (setq!
-   gptel-model 'claude-3-sonnet-20240229 ;  "claude-3-opus-20240229" also available
+   gptel-model 'claude-3-7-sonnet-20250219 ;  "claude-3-opus-20240229" also available
    gptel-backend (gptel-make-anthropic
                   "Claude"
                   :stream t
