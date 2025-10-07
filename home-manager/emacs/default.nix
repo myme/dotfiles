2dddb72fbcc08ea282d9c98be01b0c3b4c97bc0d
Martin Myrseth <mm@myme.no>
2025-10-07 10:15:06 +0200

    emacs: pgtk on Wayland on WSL has several issues

--- home-manager/emacs/default.nix
+++ home-manager/emacs/default.nix
@@ -17,7 +17,8 @@ let
   '') else null;
   flavor = specialArgs.nixosConfig.myme.machine.flavor;
   deVariant = specialArgs.nixosConfig.myme.machine.de.variant;
-  isWayland = flavor == "wsl" || builtins.elem deVariant [ "gnome" "hyprland" ];
+  # isWayland = flavor == "wsl" || builtins.elem deVariant [ "gnome" "hyprland" ];
+  isWayland = builtins.elem deVariant [ "gnome" "hyprland" ];
   EDITOR = if specialArgs.nixosConfig.myme.machine.role == "server" then
     "${et}/bin/et"
   else
