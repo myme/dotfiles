709fc1a0334a1134297d8b0e69b4796620a65bea
Martin Myrseth <mm@myme.no>
2026-03-12 09:49:14 +0100

    hyprgrab: Only invoke satty on region capture

--- pkgs/hyprgrab/default.nix
+++ pkgs/hyprgrab/default.nix
@@ -3,8 +3,15 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "hyprgrab" ''
-  ${pkgs.hyprshot}/bin/hyprshot --mode ''${1:-region} --raw |
-    ${pkgs.satty}/bin/satty \
-      --filename - \
-      --output-filename "$HOME/Pictures/Screenshots/%Y-%m-%d-%H%M%S.png"
+  mode="''${1:-region}"
+  output="$HOME/Pictures/Screenshots/$(date +%Y-%m-%d-%H%M%S).png"
+
+  if [ "$mode" = "output" ]; then
+    ${pkgs.hyprshot}/bin/hyprshot --mode output --output-folder "$(dirname "$output")" --filename "$(basename "$output")"
+  else
+    ${pkgs.hyprshot}/bin/hyprshot --mode "$mode" --raw |
+      ${pkgs.satty}/bin/satty \
+        --filename - \
+        --output-filename "$output"
+  fi
 ''
