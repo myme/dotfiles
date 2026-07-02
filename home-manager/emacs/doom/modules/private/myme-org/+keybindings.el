6829048db85f55611a049c291f2fde6609d7b064
Martin Myrseth <mm@myme.no>
2026-07-02 09:56:35 +0200

    emacs: Use "O" as org prefix

--- home-manager/emacs/doom/modules/private/myme-org/+keybindings.el
+++ home-manager/emacs/doom/modules/private/myme-org/+keybindings.el
@@ -4,8 +4,7 @@
  (:prefix "C-c"
   :desc "Org store link" :nv "l" #'org-store-link)
  (:leader
-  (:prefix "o"
-   (:prefix "o" :desc "Org Agenda"
+  (:prefix "O" :desc "Org"
     :desc "Org Agenda" :nv "a" #'org-agenda
     :desc "Org Clock In" :nv "i" #'myme/org-clock-in-recent
     :desc "Org Clock Out" :nv "o" #'org-clock-out
@@ -14,9 +13,8 @@
     :desc "Visit all org-mode files" :nv "r" #'myme/org-element-cache-reset-all
     :desc "Open Org-Roam UI" :nv "u" #'myme/org-roam-ui-open
     :desc "Stop Org-Roam UI" :nv "U" (lambda () (interactive) (org-roam-ui-mode 0))
-    :desc "Visit all org-mode files" :nv "v" #'myme/org-visit-all)
-   (:prefix "s" :desc "Org Sync"
-    (:desc "Org sync all" :nv "s" #'myme/org-auto-sync)))
+    :desc "Visit all org-mode files" :nv "v" #'myme/org-visit-all
+    :desc "Org sync all" :nv "s" #'myme/org-auto-sync)
   (:prefix
    "n r"
    (:desc "Daily from element at point" :nv "c" #'myme/org-copy-and-capture-to-daily))))
